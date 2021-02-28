9eb4e47274607e35f6c70100a67ad4cad6d156c8
micaela18 <micaela_dayana18@hotmail.com>
2021-02-28 10:10:26 +0100

    Ejercicio_VideojuegoJPDATA utilizando xampp

---
+++ Ejercicio_VideojuegoJPDATA/readme.md
@@ -0,0 +1,9 @@
+
+Hacer este ejemplo (CRUD) con la entidad Videojuego
+Un videojuego tendrá los siguientes atributos 
+
+    id
+    nombre
+    compañia
+    precio 
+    puntuacion (nota del videojuego)
--- Ejercicio_VideojuegoJPDATA/src/main/java/demo/EjercicioVideojuegoJpdataApplication.java
+++ Ejercicio_VideojuegoJPDATA/src/main/java/demo/EjercicioVideojuegoJpdataApplication.java
@@ -1,5 +1,9 @@
 package demo;
 
+import java.util.List;
+import java.util.Optional;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -22,6 +26,63 @@ public class EjercicioVideojuegoJpdataApplication {
 		v1.setPuntuacion(5);
 		
 		daoVideojuego.save(v1);
+		
+		//*****************************************************
+		Videojuego v2 = new Videojuego();
+		v2.setNombre("Mario Bros");
+		v2.setCompania("NINTENDO");
+		v2.setPrecio(49.99);
+		v2.setPuntuacion(4);
+		
+		daoVideojuego.save(v2);
+		
+		//*****************************************************
+		Videojuego v3 = new Videojuego();
+		v3.setNombre("FORTNITE");
+		v3.setCompania("EPIC");
+		v3.setPrecio(59.99);
+		v3.setPuntuacion(5);
+		
+		//daoVideojuego.save(v3);
+		
+		
+		
+		//MODIFICACIÓN
+		
+		Videojuego v4 = new Videojuego();
+		v4.setId(4);
+		v4.setNombre("GTA");
+		v4.setCompania("Rockstar Games");
+		v4.setPrecio(69.99);
+		v4.setPuntuacion(5);
+		//para realizar este cambio es necasario comentar la linea 42 ya que es el objeto a modificar, de lo contrario habría confusión 
+		daoVideojuego.save(v4);
+
+		//MÉTODO PARA ACCEDER  A UN VIDEOJUEGO 
+		
+		Optional<Videojuego> vjOptional = daoVideojuego.findById(3);
+		if (vjOptional.isPresent()) {
+			System.out.println("existe el videojuego");
+			System.out.println(vjOptional.get());
+
+		} else {
+			System.out.println("El videojuego no existe");
+		}
+		
+		// recuperamos todos los videojuegos
+		List<Videojuego> listaVj = daoVideojuego.findAll();
+		System.out.println(listaVj);
+		
+		
+		//Borra un videojuego
+		daoVideojuego.deleteById(2);
+		
+		listaVj = daoVideojuego.findAll();
+		System.out.println(listaVj);
+		
+		
+		
+		
 	}
 
 }
